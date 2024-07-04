3621c6c586a322aa8cc10e37404400cd35c66b2d
AnsaganK <ansagankabdolla4@gmail.com>
2024-07-05 00:39:23 +0600

    added new fonts, recommendation

--- app/views/flat.py
+++ app/views/flat.py
@@ -11,32 +11,32 @@ from constants import DOMAIN
 
 @login_required
 def home(request):
-    from yookassa import Configuration, Payment
-
-    Configuration.account_id = '413522'
-    Configuration.secret_key = 'test_SJH_u2rTGYZT6z4UgMc58tWN9p1MsyiOSC2S7H1nWgQ'
-
-    payments = Payment.list().items
-    for index, payment in enumerate(payments, start=1):
-        print(index, payment.id, payment.test, payment.amount.value, payment.paid)
-    print(payments)
-    payment = Payment.create({
-        "amount": {
-            "value": "2.00",
-            "currency": "RUB"
-        },
-        "payment_method_data": {
-            "type": "bank_card"
-        },
-        "confirmation": {
-            "type": "redirect",
-            "return_url": "https://www.example.com/return_url"
-        },
-        "description": "Заказ №72"
-    })
-    print(payment.__dict__)
-    print(payment.confirmation.confirmation_url)
-    print(payment)
+    # from yookassa import Configuration, Payment
+    #
+    # Configuration.account_id = '413522'
+    # Configuration.secret_key = 'test_SJH_u2rTGYZT6z4UgMc58tWN9p1MsyiOSC2S7H1nWgQ'
+    #
+    # payments = Payment.list().items
+    # for index, payment in enumerate(payments, start=1):
+    #     print(index, payment.id, payment.test, payment.amount.value, payment.paid)
+    # print(payments)
+    # payment = Payment.create({
+    #     "amount": {
+    #         "value": "2.00",
+    #         "currency": "RUB"
+    #     },
+    #     "payment_method_data": {
+    #         "type": "bank_card"
+    #     },
+    #     "confirmation": {
+    #         "type": "redirect",
+    #         "return_url": "https://www.example.com/return_url"
+    #     },
+    #     "description": "Заказ №72"
+    # })
+    # print(payment.__dict__)
+    # print(payment.confirmation.confirmation_url)
+    # print(payment)
 
 
     # template_name = 'app/email/activate.html'
