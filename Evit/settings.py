919c8ba09389001015fcb9651ee33cd26e4e03e0
Wilmar <pooyout33@gmain.com>
2024-08-11 01:04:13 +0700

    new-db-postgres

--- Evit/settings.py
+++ Evit/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'Evit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'evit_database',
+        'USER': 'evit-user',
+        'PASSWORD': 'poop2138',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
